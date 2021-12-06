987531f52b5a3ab492f0ed60447f7e9052733fa7
moke <kevin.moeri@students.unibe.ch>
2021-12-06 20:44:37 +0100

    removed build folder

--- build-SudokuSolver-Desktop-Debug/CMakeFiles/SudokuSolver_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SudokuSolver_autogen"
-  "SudokuSolver_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SudokuSolver_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
